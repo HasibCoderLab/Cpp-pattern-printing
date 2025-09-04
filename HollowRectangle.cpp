a7db5d095965a23f0a4e3e706509a5ae1ee530cd
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-04 23:15:25 +0600

    create a file for solve problelm

---
+++ HollowRectangle.cpp
@@ -0,0 +1,16 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic   : [pattern-printing]
+//               Problem : 👉👉 [18]
+//               Want    : HollowRectangle
+// ================================================
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+    cout << "Enter rows and columns: ";
+    cin >> rows >> cols;
+
+    return 0;
+}
