49e2b87aa9b8262d1edf16bb056a42429a448be9
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-04 23:56:35 +0600

    solve 20th  problem

--- StarTriangleReverse.cpp
+++ StarTriangleReverse.cpp
@@ -1,6 +1,32 @@
 // ================================================
 //               Language: C++ / Cpp
 //               Topic   : [pattern-printing]
-//               Problem : 👉👉 [18]
+//               Problem : 👉👉 [20]
 //               Want    : Star Triangle Teverse
 // ================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<"*";
+        }
+        cout << endl;
+    }
+   
+
+    return 0;
+}
