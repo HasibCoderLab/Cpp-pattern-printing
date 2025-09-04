e540251226f3d104300176b1bf88f892077fdf49
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-04 22:02:47 +0600

    solve 14th problem of Cpp-pattern-printing in C++

--- OddNumberTriangle.cpp
+++ OddNumberTriangle.cpp
@@ -1,15 +1,25 @@
 // ================================================
 //               Language: C++ / Cpp
 //               Topic   : [pattern-printing]
-//               Problem : 👉👉 [10]
+//               Problem : 👉👉 [14]
 //               Want    : OddNumberTriangle
-//                        
+//
 // ================================================
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int n;
-    cout<< "Enter number " ;
-    cin>> n;
-    return 0 ;
+    cout << "Enter number ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << j * 2 - 1 << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
