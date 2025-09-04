094403178d8d025219db79318a040dfa71695053
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-04 23:28:50 +0600

    solve 18th problem of Cpp-pattern-printing in C++

--- HollowRectangle.cpp
+++ HollowRectangle.cpp
@@ -11,6 +11,20 @@ int main() {
     int rows, cols;
     cout << "Enter rows and columns: ";
     cin >> rows >> cols;
+    for(int i= 1 ; i<=rows ; i++){
+        for(int j = 1 ; j<=cols;j++){
+           if (i== 1 || i==rows || j == 1 || j== cols)
+           {
+            cout<<" * " ;
+           }else
+           {
+            cout<<" " ;
+           }
+           
+           
+        }
+        cout<<endl;
+    }
 
     return 0;
 }
