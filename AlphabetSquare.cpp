c1fdcdc80ff18f78f6f2a3c4d056e68acd97bba5
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-04 17:23:30 +0600

    solve 4th  problem of Cpp-pattern-printing in C++

--- AlphabetSquare.cpp
+++ AlphabetSquare.cpp
@@ -8,7 +8,15 @@
 #include <iostream>
 using namespace std;
 int main(){
+    int n ;
+    cout<<"Enter side of Square " ;
+    cin>> n;
+    for(int i  = 1 ; i<=n; i++){
+        for(int j = 1 ; j<=n ; j++){
+            cout<<(char)(j+64);
+
+        }
+        cout<<endl;
+    }
     return 0 ;
 }
-
-solve 1st problem of Cpp-pattern-printing in C++
