a9f701c2c56b5ee3f568a221b5a762f0fbfa2702
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-04 17:13:06 +0600

    solve 3rd problem of Cpp-pattern-printing in C++

---
+++ NumberSquare.cpp
@@ -0,0 +1,23 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic   : [pattern-printing]
+//               Problem : 👉👉 [3]
+//               Want    : NumberSquare
+//
+// ================================================
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    ;
+    cout << "Enter Number ";
+    cin >> n;
+   for(int i = 1 ;i<=n ; i++){
+    for(int  j = 1; j <=n ; j++){
+        cout<< j <<" " ; 
+    }
+    cout<< endl;
+   }
+    return 0;
+}
