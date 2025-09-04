3b22f8d1a302b46e4bf3d145f29c81b5edc35a41
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-04 21:49:50 +0600

    solve 13th problem of Cpp-pattern-printing in C++

--- ReverseNumberTriangle.cpp
+++ ReverseNumberTriangle.cpp
@@ -10,5 +10,11 @@ int main(){
     int n;
     cout << "Enter number: ";
     cin >> n;
+    for(int i = 1 ; i<=n ; i++){
+        for(int j = n ; j>=n-i+1; j--){
+               cout<<j;
+        }
+        cout<<endl;
+    }
 return 0;
 }
