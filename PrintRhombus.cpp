ba950bf1b51f97d4300609c9f8457511dc5941ba
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-05 00:00:00 +0600

    create a file for solve problelm

---
+++ PrintRhombus.cpp
@@ -0,0 +1,26 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic   : [pattern-printing]
+//               Problem : 👉👉 [20]
+//               Want    : Star Triangle Teverse
+// ================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+   for(int i = 1 ; i<=n;i++){
+    for(int j = 1; j<=n+i;j++){
+            cout<<" " ;
+    }
+    for(int j = 1; j<=i;j++){
+    
+    }
+   }
+
+    return 0;
+}
