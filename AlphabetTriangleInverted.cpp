6364e2671f837a3bd30fd244103bdc28862681d5
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-04 21:31:37 +0600

    create a file for solve problem

---
+++ AlphabetTriangleInverted.cpp
@@ -0,0 +1,21 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic   : [pattern-printing]
+//               Problem : 👉👉 [10]
+//               Want    : AlphabetTriangleInverted
+//                        
+// ================================================
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<< "Enter number " ;
+    cin>> n;
+    for(int i = 1 ; i<= n ; i++){
+        for(int j = 1 ; j<=n -i +1;j++){
+                   cout<< j;
+        }
+        cout<<endl;
+    }
+    return 0 ;
+}
